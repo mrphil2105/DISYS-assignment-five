44ef138901c62e1a9f32ba4fe3d4acd5405f9172
Philip Mørch <mrphil2105@gmail.com>
2022-12-20 15:39:35 +0100

    Add util function for connecting to Ring neighbor

--- util.go
+++ util.go
@@ -24,6 +24,19 @@ func (*Server) ConnectBackupClient(port string) (auction.ConnectServiceClient, a
 	return connectClient, auctionClient
 }
 
+func (*Server) ConnectRingClient(port string) auction.RingServiceClient {
+	log.Printf("Dialing ring node on port %s", port)
+
+	conn, err := grpc.Dial(net.JoinHostPort("localhost", port),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to dial ring node: %v", err)
+	}
+
+	return auction.NewRingServiceClient(conn)
+}
+
 func (*Frontend) ConnectServerClient(port string) auction.AuctionServiceClient {
 	log.Printf("Dialing server on port %s", port)
 
