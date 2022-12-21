ef11438b6376f9f09503fdc232289f4df7aa45f7
Philip Mørch <mrphil2105@gmail.com>
2022-12-21 19:34:37 +0100

    Extract bet and result commands to separate functions

--- client.go
+++ client.go
@@ -33,43 +33,51 @@ func client() {
 
 		switch input[0] {
 		case "bet":
-			amount, err := strconv.ParseUint(input[1], 10, 32)
-
-			if err != nil {
-				log.Printf("Unable to parse '%s' as amount: %v", input[1], err)
-				continue
-			}
-
-			bid := &auction.Bid{
-				Pid:    client.pid,
-				Amount: uint32(amount),
-			}
-			ack, err := client.frontendClient.SendBid(context.Background(), bid)
-
-			if err != nil {
-				log.Fatalf("Unable to place bid: %v", err)
-			}
-
-			if ack.GetSuccess() {
-				log.Printf("Bid of '%d' successfully placed", amount)
-			} else {
-				log.Printf("Bid of '%d' was not higher than the highest bid", amount)
-			}
+			SendBid(client, input)
 		case "result":
-			outcome, err := client.frontendClient.GetResult(context.Background(), &auction.Void{})
-
-			if err != nil {
-				log.Fatalf("Unable to get auction outcome: %v", err)
-			}
-
-			log.Printf("Highest bid: %d, winner pid: %d", outcome.GetHighestBid(), outcome.GetWinner())
-			log.Printf("Bidders:")
-
-			for _, bidder := range outcome.GetBidders() {
-				log.Printf("  %d", bidder)
-			}
+			GetResult(client)
 		default:
 			fmt.Printf("Unknown command '%s'\n", input[0])
 		}
+	}
+}
+
+func SendBid(client *Client, input []string) {
+	amount, err := strconv.ParseUint(input[1], 10, 32)
+
+	if err != nil {
+		log.Printf("Unable to parse '%s' as amount: %v", input[1], err)
+		return
+	}
+
+	bid := &auction.Bid{
+		Pid:    client.pid,
+		Amount: uint32(amount),
+	}
+	ack, err := client.frontendClient.SendBid(context.Background(), bid)
+
+	if err != nil {
+		log.Fatalf("Unable to place bid: %v", err)
+	}
+
+	if ack.GetSuccess() {
+		log.Printf("Bid of '%d' successfully placed", amount)
+	} else {
+		log.Printf("Bid of '%d' was not higher than the highest bid", amount)
+	}
+}
+
+func GetResult(client *Client) {
+	outcome, err := client.frontendClient.GetResult(context.Background(), &auction.Void{})
+
+	if err != nil {
+		log.Fatalf("Unable to get auction outcome: %v", err)
+	}
+
+	log.Printf("Highest bid: %d, winner pid: %d", outcome.GetHighestBid(), outcome.GetWinner())
+	log.Printf("Bidders:")
+
+	for _, bidder := range outcome.GetBidders() {
+		log.Printf("  %d", bidder)
 	}
 }
